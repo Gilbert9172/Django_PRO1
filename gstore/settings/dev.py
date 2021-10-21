8199a2fad14036dc3ba2302d600ef2b51b31a608
GILBERT9172 <gilbert9172@naver.com>
2021-10-21 09:32:10 +0900

    21.10.21

--- gstore/settings/dev.py
+++ gstore/settings/dev.py
@@ -8,4 +8,8 @@ MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware",] + MIDDLEWARE
 
 INTERNAL_IPS = ["127.0.0.1"]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'gstore' ,'static')
+]
+
 CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
